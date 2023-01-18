d593cb37641546ec0d4c73b0e068ac373c90095c
Daniel Conte <182736@upf.br>
2023-01-18 17:15:14 -0300

    cria header e resumo dos dias

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Habit from "./components/Habit";
 import "./styles/global.css";
+import Header from "./components/Header";
+import SummaryTable from "./components/SummaryTable";
 
 function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={1} />
-      <Habit completed={2} />
-      <Habit completed={4} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,42 @@
+import generateDatesFromYearBeginning from "../utils/generate-dates-from-year-beginning";
+import HabitDay from "./HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDates = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDates - summaryDates.length;
+
+function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => (
+          <div
+            key={`${weekDay}-${index}`}
+            className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default SummaryTable;
