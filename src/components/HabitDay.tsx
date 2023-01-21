546f0e171eed8f4e078eb35024cfb50e472a75eb
Daniel Conte <182736@upf.br>
2023-01-21 15:08:27 -0300

    conecta api com lista de habitos do dia e toggle

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,22 +1,28 @@
 import * as Popover from "@radix-ui/react-popover";
-import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import ProgressBar from "./ProgressBar";
-import { Check } from "phosphor-react";
 import dayjs from "dayjs";
+import HabitsList from "./HabitsList";
+import { useState } from "react";
 
 interface HabitDayProps {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   amount?: number;
 }
 
-function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
+function HabitDay({ date, amount = 0, defaultCompleted = 0 }: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format("DD/MM");
   const dayOfWeek = dayjs(date).format("dddd");
+
+  function handleAmountCompletedChanged(completed: number) {
+    setCompleted(completed);
+  }
 
   return (
     <Popover.Root>
@@ -50,19 +56,10 @@ function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList
+            date={date}
+            onCompletedChanged={handleAmountCompletedChanged}
+          />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
